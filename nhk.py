ab3a3b37ac9330c0bf8bbc61fe3578c5d2afbf6d
Daniel Lemm <61800298+ffe4@users.noreply.github.com>
2021-04-12 13:33:11 +0200

    Add NHKResult and NHK.lookup

--- nhk.py
+++ nhk.py
@@ -1,4 +1,25 @@
 import xml.etree.ElementTree as ET
+from dataclasses import dataclass
+
+
+@dataclass
+class NHKResult:
+    title: str
+    summary: str
+    head: str
+    body: str
+
+    @classmethod
+    def from_xml_element(cls, element):
+        title = element.find("title").text
+        summary = element.find("summary").text
+        head_element = element.find("head")
+        head_element.tag = "div"
+        head = "".join([ET.tostring(e, encoding="unicode") for e in list(head_element)])
+        body_element = element.find("body")
+        body_element.tag = "div"
+        body = "".join([ET.tostring(e, encoding="unicode") for e in list(body_element)])
+        return cls(title, summary, head, body)
 
 
 class NHK:
@@ -7,14 +28,20 @@ class NHK:
         self.root = self.tree.getroot()
 
     def find_items(self, query):
-        for item in self.root.iter('dic-item'):
-            if item.get('type'):
+        for item in self.root.iter("dic-item"):
+            if item.get("type"):
                 # filters 凡例 and 外字一覧 items
                 continue
-            keys = item.find('keys')
-            for key in keys.iter('key'):
+            keys = item.find("keys")
+            for key in keys.iter("key"):
                 if key.text == query:
                     yield item
+
+    def lookup(self, query):
+        results = []
+        for item in self.find_items(query):
+            results.append(NHKResult.from_xml_element(item))
+        return results
 
 
 def lookup(query):
--- test_nhk.py
+++ test_nhk.py
@@ -2,12 +2,26 @@ import pytest
 
 from nhk import NHK
 
-nhk = NHK('data/nhk_dict.xml')
+nhk = NHK("data/nhk_dict.xml")
 
 
-@pytest.mark.parametrize('query,expected_ids', [
-    ('間', {'160', '1010', '109670', '500750'})
-])
+@pytest.mark.parametrize(
+    "query,expected_ids", [("間", {"160", "1010", "109670", "500750"})]
+)
 def test_find_items(query, expected_ids):
-    actual_ids = {item.get('id') for item in nhk.find_items(query)}
+    actual_ids = {item.get("id") for item in nhk.find_items(query)}
     assert actual_ids == expected_ids
+
+
+def test_lookup():
+    query = "間"
+    expected_titles = {
+        "愛，相〔接〕，あい《間》（間隔），藍，合い（～の洋服）",
+        "間",
+        "官，巻，感，缶，観，間，棺，款，管，環，漢，冠（～たる），かん《緘，鐶》，歓，閑，艦，かん《奸》（君側の～）",
+        "間（室，拍子），真（～に受ける）",
+    }
+    results = nhk.lookup(query)
+    actual_titles = {result.title for result in results}
+    assert len(results) == 4
+    assert actual_titles == expected_titles
